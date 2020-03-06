f1f7b09dcb981b7dd123dac921d1283464ed0055
rezaiyan <alirezaiyann@gmail.com>
2020-03-06 09:29:04 +0330

    Application class created

---
+++ app/src/main/java/ir/alirezaiyan/arzte/Arzte.kt
@@ -0,0 +1,14 @@
+package ir.alirezaiyan.arzte
+
+import android.app.Application
+
+/**
+ * @author Ali (alirezaiyann@gmail.com)
+ * @since 3/6/2020 2:25 AM.
+ */
+class Arzte : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
